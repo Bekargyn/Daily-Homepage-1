93da5a7a2b85c18993119bd6cb0a9fd143c5e1d2
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-01-07 13:12:45 -0600

    added apikey

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ const weatherApiKey = "7bb4b8b4c54f73c9621bee4e6c4a3bf9";
 // TOMTOM's API key
 const eventApiKey = "kecAu5C3f80LkEgYXGQvZgfLk0dpZGE4";
 // Ticketmaster's API key utNZSTGMX1zeTwLA5z5ppyXFAxACrTrb
+
+// time and date - BA
