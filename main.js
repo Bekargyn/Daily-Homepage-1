1141f5e0b2bb03cde34bf8c5ba8d0be61edab640
Jake Walker <jcw2865@gmail.com>
2020-01-08 12:41:07 -0600

    Displayed news article buttons in HTML column for news.

--- main.js
+++ main.js
@@ -10,24 +10,24 @@ $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function (response) {
-  console.log(response);
+  // console.log(response);
 
-  // Create a container for the articles
-  var articleContainer = $("<div id='articleContainer'>");
-  articleContainer.css("width", "300px");
+  // // Create a container for the articles
+  // var articleContainer = $("<div id='articleContainer'>");
+  // articleContainer.css("width", "300px");
 
-  // Append article container to body
-  $("body").append(articleContainer);
+  // // Append article container to body
+  // $("body").append(articleContainer);
 
   // Loop through the array of articles
   for (var i = 0; i < response.articles.length; i++) {
 
-    // Create div for article
+    // Create button for article
     var articleButton = $("<button class='article'>");
     articleButton.attr("href", response.articles[i].url)
 
-    // Append article div to article container
-    articleContainer.append(articleButton);
+    // Append article button to article container
+    $("#articleContainer").append(articleButton);
     articleButton.css("border-style", "solid");
 
     // Store article title in a variable
@@ -45,8 +45,10 @@ $.ajax({
     // Create an element to hold the image
     var image = $("<img>").attr("src", imgURL);
     image.css("height", "100px");
+    image.css("float", "bottom");
 
     // Append the image element to the title element
+    pTitle.append("<br>");
     pTitle.append(image);
   }
   $(document).on('click', 'button', function (e) {
